db4d5b38cecaef5650e2466b34e7084d1c2bfbad
Ian <ianmcm.89@gmail.com>
2021-08-20 12:24:30 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function writeCards(array, eventName) {
+    const newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        let message =  `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+const countDown = i => { while(i >= 0) { console.log(i); i--; }};
